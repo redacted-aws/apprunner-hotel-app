bc3eafcc64acac944749883d9d3a1b4df3fbbaaa
Srijit <srijit@amazon.com>
2022-10-13 14:01:18 -0400

    xray fixes

--- create-a-tracer.js
+++ create-a-tracer.js
@@ -5,7 +5,7 @@ const { trace } = require('@opentelemetry/api');
 
 // OTel JS - Core
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { ConsoleSpanExporter, BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 
 // OTel JS - Core - Exporters
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
@@ -14,18 +14,21 @@ const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc')
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { MySQLInstrumentation } = require('@opentelemetry/instrumentation-mysql');
 const { AwsInstrumentation } = require('@opentelemetry/instrumentation-aws-sdk');
+const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions')
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 
 // OTel JS - Contrib - AWS X-Ray
 const { AWSXRayIdGenerator } = require('@opentelemetry/id-generator-aws-xray');
 const { AWSXRayPropagator } = require('@opentelemetry/propagator-aws-xray');
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+
 
 registerInstrumentations({
   instrumentations: [
     new HttpInstrumentation(),
     new MySQLInstrumentation(),
+    new ExpressInstrumentation(),
     new AwsInstrumentation({
       suppressInternalInstrumentation: true
     }),
@@ -36,7 +39,7 @@ const resource =
   Resource.default().merge(
     new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: "apprunner-hotel-app",
-      [SemanticResourceAttributes.SERVICE_VERSION]: ".1.0",
+      [SemanticResourceAttributes.SERVICE_VERSION]: "3.0.0",
     })
   );
 
