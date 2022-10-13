f29ee5d1167914d031f4303b2f371620496c2a7c
Srijit <srijit@amazon.com>
2022-10-13 14:55:56 -0400

    xray

--- create-a-tracer.js
+++ create-a-tracer.js
@@ -1,9 +1,11 @@
+'user strict';
+
 // OTel JS - API
 const { trace } = require('@opentelemetry/api');
 
 // OTel JS - Core
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 
 // OTel JS - Core - Exporters
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
@@ -13,14 +15,16 @@ const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { MySQLInstrumentation } = require('@opentelemetry/instrumentation-mysql');
 const { AwsInstrumentation } = require('@opentelemetry/instrumentation-aws-sdk');
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
-const { Resource } = require( '@opentelemetry/resources');
+const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 
 // OTel JS - Contrib - AWS X-Ray
 const { AWSXRayIdGenerator } = require('@opentelemetry/id-generator-aws-xray');
 const { AWSXRayPropagator } = require('@opentelemetry/propagator-aws-xray');
 
+// Debug Logging
+const { DiagConsoleLogger, DiagLogLevel, diag } = require('@opentelemetry/api');
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 module.exports = (serviceName) => {
   // create a provider using the AWS ID Generator
@@ -45,7 +49,7 @@ module.exports = (serviceName) => {
 
   // add OTLP exporter
   const otlpExporter = new OTLPTraceExporter();
-  tracerProvider.addSpanProcessor(new BatchSpanProcessor(otlpExporter));
+  tracerProvider.addSpanProcessor(new SimpleSpanProcessor(otlpExporter));
 
   // Register the tracer provider with an X-Ray propagator
   tracerProvider.register({
